3fcdf46326e3f1d4e2d2d36c70155721e128e708
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2024-01-01 00:33:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=65;j<=n+65-1;j++)         {          printf("%c ",j);         }         printf(" ");     } }

---
+++ Basic_Pattern_1.4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=65;j<=n+65-1;j++)
+        {
+         printf("%c ",j);   
+        }
+        printf("
+");
+    }
+}
