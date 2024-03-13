27c3da2d82c9f04d3c599413e4eebeef143f3458
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2024-03-13 21:17:18 +0530

    #include<stdio.h> int dc(int n) {     int cnt = 0;     while(n!=0)     {         n=n/10;         cnt+=1;     }     if(cnt%2==0)     {         return 1;     }     else     {         return 0;     } } int main() {     int n,count = 0;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         if(dc(arr[i]) == 1)         {             count+=1;         }     }     printf("%d",count); }

--- Find_Numbers_with_Even_Number_of_Digits.c
+++ Find_Numbers_with_Even_Number_of_Digits.c
@@ -1,29 +1,36 @@
 #include<stdio.h>
 int dc(int n)
 {
-    int count = 0;
-    while (n!=0)
+    int cnt = 0;
+    while(n!=0)
     {
-        n = n/10;
-        count += 1;
+        n=n/10;
+        cnt+=1;
     }
-    return count;
+    if(cnt%2==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-int main(){
-    int n,cnt=0; 
-    scanf("%d", &n);
+int main()
+{
+    int n,count = 0;
+    scanf("%d",&n);
     int arr[n];
-    for(int i=0;i< n ;i++)
+    for(int i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(int i=0;i< n ;i++)
+    for(int i=0;i<n;i++)
     {
-        if (dc(arr[i])%2==0)
+        if(dc(arr[i]) == 1)
         {
-            ++cnt;
+            count+=1;
         }
     }
-    printf("%d", cnt);
+    printf("%d",count);
 }
-    
